58df08ad79b405c3121b237c129b6804ecbcdb31
goto <gotomsak@gmail.com>
2016-08-29 17:35:38 +0900

    スタートボタンのコードを見やすくした

--- app/src/main/java/com/example/a1621098/PepperTimer/PepperTimer.java
+++ app/src/main/java/com/example/a1621098/PepperTimer/PepperTimer.java
@@ -1,4 +1,4 @@
-package com.example.a1621098.layoutpractice;
+package com.example.a1621098.PepperTimer;
 
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
@@ -8,11 +8,9 @@ import android.widget.Button;
 import android.widget.TextView;
 import com.aldebaran.qi.sdk.object.interaction.Say;
 
-public class Timer extends AppCompatActivity {
-
+public class PepperTimer extends AppCompatActivity {
     private Button startButton, stopButton;
     private TextView timerText;
-
     public int count = 0;
     public long pause =0;
     public long millis;
@@ -26,44 +24,36 @@ public class Timer extends AppCompatActivity {
         setContentView(R.layout.activity_layout);
         startButton = (Button)findViewById(R.id.start_button);
         stopButton = (Button)findViewById(R.id.stop_button);
-
         timerText = (TextView)findViewById(R.id.timer);
-        timerText.setTextSize(timerText.getTextSize()*1.5f);
         timerText.setText("05:00");
-
-        // インスタンス生成
         // CountDownTimer(long millisInFuture, long countDownInterval)
         final CountDown[] countDown = {new CountDown(Timer)};
 
-         say= new Say(this);
+        say= new Say(this);
 
         startButton.setOnClickListener(new View.OnClickListener() {     //スタートボタンの処理
             @Override
-
             public void onClick(View v) {           //スタートボタンを押したとき
                 count++;
-                if(count%2==1) {                    // 開始
+                bunki(count);
+            }
+            private void bunki(int count){
+                if(count%2==1) {// 開始
+                    bunki2(count);
                     say.run("スタート");
-                    if(count > 1) {
-                        countDown[0] = new CountDown(pause);
-                        countDown[0].start();
-                    }else{countDown[0].start();}
-
-                    Button startButton = (Button) v;
                     startButton.setText("pause");
-
                 }else if(count%2==0){               //一時停止
-
-                   pause=millis;
-
+                    pause=millis;
                     say.run("停止");
                     countDown[0].cancel();
-
-                    Button startButton = (Button) v;
                     startButton.setText("start");
-
                 }
-
+            }
+            private void bunki2(int count){
+                if(count > 1) {
+                    countDown[0] = new CountDown(pause);
+                    countDown[0].start();
+                }else{countDown[0].start();}
             }
         });
         //タイマーリセット
@@ -76,26 +66,24 @@ public class Timer extends AppCompatActivity {
                 // 中止
                 countDown[0].cancel();
                 countDown[0] = new CountDown(Timer);
-
                 startButton.setText("start");
-
                 timerText.setText("05:00");
                 startButton.setVisibility(View.VISIBLE);
 
             }
         });
+
     }
     public  long  from_min_to_msec(long min){
         return min*60*1000;
     }
 
     class CountDown extends CountDownTimer {                    //タイマーのクラス
-         public long countMillis = -1;
+        public long countMillis = -1;
         //時計関係のやつ　
         public CountDown(long millisInFuture) {
             super(millisInFuture, 100);
         }
-
 
         @Override
         public void onTick(long millisUntilFinished) {          //残り時間の表示
@@ -119,5 +107,4 @@ public class Timer extends AppCompatActivity {
             return countMillis;
         }
     }
-
 }
